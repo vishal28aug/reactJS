56807db3464924021487b0625a4acbfad168a104
unknown <vishalsharma25sept@gmail.com>
2020-01-12 00:32:32 +0530

    Installed material UI and adding sidenav

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React, { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
+import SideNav from './component/side-nav/SideNav'
+import UserDescription from './component/user-description/UserDescription'
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Hello React !!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SideNav/>
+      <UserDescription/>
     </div>
     )
   }
--- src/component/side-nav/SideNav.js
+++ src/component/side-nav/SideNav.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
-import './SideNav'
+import './SideNav.css'
+import Avatar from '@material-ui/core/Avatar';
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+
 export default class SideNav extends Component {
     render() {
+
         return (
-            <div>
-                side nav stuff
+            <div className="sidebar">
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
             </div>
         )
     }
