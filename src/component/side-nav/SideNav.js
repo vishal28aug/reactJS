b0628fcc8dd6e63fe8abe643f45a6e734960b61b
unknown <vishalsharma25sept@gmail.com>
2020-01-11 23:51:13 +0530

    Added side-nav and user-description component

---
+++ src/component/side-nav/SideNav.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import './SideNav'
+export default class SideNav extends Component {
+    render() {
+        return (
+            <div>
+                side nav stuff
+            </div>
+        )
+    }
+}
---
+++ src/component/user-description/UserDescription.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './UserDescription'
+
+export default class UserDescription extends Component {
+    render() {
+        return (
+            <div>
+                User description stuff
+            </div>
+        )
+    }
+}
